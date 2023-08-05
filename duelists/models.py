1fc376974d6740526f1464c7689a1858cced0a8e
Eng. Elias <eng.elias.owis@gmail.com>
2023-08-05 14:04:55 +0300

    adding __str__ method to models

--- duelists/models.py
+++ duelists/models.py
@@ -6,8 +6,14 @@ class DuelCard(models.Model):
     description = models.TextField(max_length=1000, blank=True, null=True)
     type = models.CharField(choices=[('monster', 'Monster'), ('spell', 'Spell'), ('trap', 'Trap')], max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Duelist(models.Model):
     name = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
     favourite_card = models.ForeignKey(DuelCard, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
